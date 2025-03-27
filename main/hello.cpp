08c9ba1c18e2459a82a0e530a3bc4dbf79872f16
akash-ghatage19 <akashghatage.19@gmail.com>
2025-03-27 11:08:57 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,8 @@ int main(void) {
   
   cout << "Hello, Jenkins\n";
   cout << "Hello, PES1UG22CS059\n";
+  cout << "How are you, PES1UG22CS059\n";
+
   
   cout << "I have successfully built and run this  \n";
   return 0;
